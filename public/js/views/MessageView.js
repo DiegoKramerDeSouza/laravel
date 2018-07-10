c6b14b3cd07ff4914c2c81c6866fa8c4305a194d
Diego Kramer de Souza <diego.souza@intelit.com.br>
2018-07-10 14:46:25 -0300

    Alteração - Apresentação de elementos - Modificação das barras de rolagem

--- public/js/views/MessageView.js
+++ public/js/views/MessageView.js
@@ -7,12 +7,13 @@ class MessageView {
 
     _template(arrModelo, detail) {
 
-        return detail ? `<span class="white-text">${arrModelo[0]} ${detail} ${arrModelo[1]}</span>` : `<span class="white-text">${arrModelo[0]} ${arrModelo[1]}</span>`;
+        return detail ? `${arrModelo[0]} ${detail} ${arrModelo[1]}` : `${arrModelo[0]} ${arrModelo[1]}`;
     }
 
     update(arrModelo, value) {
 
         this._text = value ? this._template(arrModelo, value) : this._template(arrModelo);
+        this._text = `<span class="white-text">${ this._text }</span>`;
         let mensagem = new Message(this._text, arrModelo[2]);
         M.toast({ html: mensagem.text, classes: mensagem.model, displayLength: 2000 });
     }
